317a9b5499a1f40f5f0118074111d289bef96c5d
유주현 <gus6359@gmail.com>
2022-06-23 04:57:21 +0900

    feat: Welcome 반응형

--- src/components/Login/Welcome/style.jsx
+++ src/components/Login/Welcome/style.jsx
@@ -14,14 +14,18 @@ export const Wrapper = styled.div`
 
 export const ProfileImg = styled.div`
   display: flex;
-  width: 250px;
-  height: 250px;
+  max-width: 250px;
+  max-height: 250px;
+  width: 50vw;
+  height: 50vw;
   background: #d9d9d9;
   border-radius: 100%;
+  margin: auto;
   margin-bottom: 30px;
 `;
 
 export const WelcomeMsg = styled.label`
   display: flex;
   margin: auto;
+  font-size: min(18px, 3.5vw);
 `;
