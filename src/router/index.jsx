d9a860a23c78b128dc3f86da9152ebf572fd5f24
유주현 <gus6359@gmail.com>
2022-06-23 05:01:06 +0900

    feat: Manager route component

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,16 +2,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Main, Manager, User, NotFound } from 'pages';
 import { UserRoute } from 'components/User/UserRoute';
 import { Welcome } from 'components/Login/Welcome';
-
+import { ManageRoute } from 'components/Manager/ManagerRoute';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<UserRoute element={Main} showSearchBar={false} />} />
-        <Route path="/manager" element={<UserRoute element={Manager} showSearchBar={false} />} />
         <Route path="/user" element={<UserRoute element={User} showSearchBar={true} />} />
-        <Route path="/welcome" element={<UserRoute element={Welcome} showSearchBar={true} hidden={true} />} />
+        <Route path="/welcome" element={<UserRoute element={Welcome} hidden={true} />} />
+        
+        <Route path="/manager" element={<ManageRoute element={Manager} showSearchBar={false} />} />
+        
         <Route path="*" element={<UserRoute element={NotFound} showSearchBar={false} />} />
       </Routes>
     </BrowserRouter>
