80bf9b70ff87553bd5c86629743bc6f9d357df2c
유주현 <gus6359@gmail.com>
2022-06-23 04:45:34 +0900

    feat: Welcome page

--- src/components/User/UserRoute/index.jsx
+++ src/components/User/UserRoute/index.jsx
@@ -1,9 +1,9 @@
 import { Header } from 'components';
 import React from 'react';
 
-export function UserRoute({ element: Element, showSearchBar }) {
+export function UserRoute({ element: Element, showSearchBar, hidden }) {
   return (<>
-    <Header showSearchBar={showSearchBar} userType={0} />
+    <Header showSearchBar={showSearchBar} userType={0} hidden={hidden} />
     <Element />
   </>);
 }
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Main, Manager, User, NotFound } from 'pages';
 import { UserRoute } from 'components/User/UserRoute';
+import { Welcome } from 'components/Login/Welcome';
+
 
 export default function Router() {
   return (
@@ -9,6 +11,7 @@ export default function Router() {
         <Route path="/" element={<UserRoute element={Main} showSearchBar={false} />} />
         <Route path="/manager" element={<UserRoute element={Manager} showSearchBar={false} />} />
         <Route path="/user" element={<UserRoute element={User} showSearchBar={true} />} />
+        <Route path="/welcome" element={<UserRoute element={Welcome} showSearchBar={true} hidden={true} />} />
         <Route path="*" element={<UserRoute element={NotFound} showSearchBar={false} />} />
       </Routes>
     </BrowserRouter>
