226f8af28b0f9fd7a8d844068f2a99a8323ed7b6
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-06-22 22:34:30 +0900

    feat: pages 컴포넌트 구성

---
+++ src/pages/Manager/index.jsx
@@ -0,0 +1,5 @@
+import * as S from './style';
+
+export function Manager() {
+  return <S.Container>Manager</S.Container>;
+}
---
+++ src/pages/User/index.jsx
@@ -0,0 +1,5 @@
+import * as S from './style';
+
+export function User() {
+  return <S.Container>User</S.Container>;
+}
