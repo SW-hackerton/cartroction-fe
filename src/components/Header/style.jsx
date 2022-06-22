a04a264734b4624ee8fb638cf41e6879477f18b1
유주현 <gus6359@gmail.com>
2022-06-23 04:08:08 +0900

    feat: Header component

--- src/components/Header/style.jsx
+++ src/components/Header/style.jsx
@@ -1,5 +1,56 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+const contentHeight = 30;
+const getContentHeight = (height = contentHeight) => `height: ${height}px`;
+const getContentMargin = (margin = 10) => `margin: auto ${margin}px auto 0`;
 
-export const Wrapper = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 50px;
+  justify-content: center;
+  background: #e4e4e4;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: 80%;
+`;
+
+export const SearchBarWrapper = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+export const UserInfoWrapper = styled.div`
+  display: flex;
+  margin-left: auto;
+`;
+
+export const WireBox = styled.div`
+  min-width: 50px;
+  width: 10%;
+  ${getContentHeight()};
+  ${getContentMargin()};
+  background: #c4c4c4;
+`;
+
+export const WireSearchBox = styled.div`
+  min-width: 150px;
+  width: 40%;
+  ${getContentHeight()};
+  ${getContentMargin()};
+  background: #c4c4c4;
+  border-radius: 20px;
+
+  &::before {
+    content: '';
+    position: absolute;
+    width: 25px;
+    height: 25px;
+    background: #a4a4a4;
+    margin-top: calc(calc(${contentHeight}px - 25px) / 2);
+    margin-left: 5px;
+    border-radius: 20px;
+  }
+`;
