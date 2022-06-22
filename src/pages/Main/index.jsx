58a8b5bf6b2e0b1926425c7e4f76823991c393e2
유주현 <gus6359@gmail.com>
2022-06-23 04:08:48 +0900

    feat: Main page

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,5 +1,12 @@
 import * as S from './style';
 
 export function Main() {
-  return <S.Container>Main</S.Container>;
+  return (
+    <S.Container>
+      <S.Wrapper>
+        <S.WireSearchBox />
+        <S.WireSearchBox__icon />
+      </S.Wrapper>
+    </S.Container>
+  );
 }
