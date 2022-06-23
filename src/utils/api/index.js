0fca757ad5cbd649f70439c6e17d056d0d349704
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-06-23 15:54:40 +0900

    fix: axios create로 변경

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -2,10 +2,18 @@ import axios from 'axios';
 
 const BASE_URL = process.env.BASE_URL;
 
+const _axios = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+});
+
 export const api = {
   submitForm: ({ data }) => {
     try {
-      const response = axios.post(`${BASE_URL}/manager/register`, data);
+      const response = _axios.post('/api/submitForm', data);
 
       if (response.status !== 200) {
         throw new Error('Error');
@@ -18,7 +26,7 @@ export const api = {
   },
   getResult: ({ param }) => {
     try {
-      const response = axios.get(`${BASE_URL}/${param}`);
+      const response = _axios.get(`/${param}`);
 
       if (response.status !== 200) {
         throw new Error('Error');
