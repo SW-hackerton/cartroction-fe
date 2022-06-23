1e3330f7b81ea8289a011ae337303aa03d3bffbe
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-06-23 14:18:52 +0900

    feat: result 적용

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,29 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from 'components';
-import { Main, Manager, User, NotFound } from 'pages';
+import { Main, Manager, User, NotFound, CarResult } from 'pages';
 import { Welcome } from 'components/Login/Welcome';
-import { ManageRoute } from 'components/Manager/ManagerRoute';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/result" element={<CarResult />} />
         <Route path="/manager" element={<Manager />} />
         <Route path="/user" element={<User />} />
-        {/* =======
-        <Route
-          path="/"
-          element={<UserRoute element={Main} showSearchBar={false} />}
-        />
-        <Route
-          path="/manager"
-          element={<ManageRoute element={Manager} showSearchBar={false} />}
-        />
-        <Route
-          path="/user"
-          element={<UserRoute element={User} showSearchBar={true} />}
-        /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/welcome" element={<Welcome />} />
         <Route path="*" element={<NotFound />} />
