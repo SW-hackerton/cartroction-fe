e99ef4104417ac3d9577906817a6761b00343342
유주현 <gus6359@gmail.com>
2022-06-23 16:22:08 +0900

    feat: Main page design 적용

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,16 +1,16 @@
-import { TimeLineList, NavTemplate } from 'components';
 import * as S from './style';
 
 export function Main() {
   return (
-    <NavTemplate isSearchBar={false}>
-      <S.Container>
-        <S.Wrapper>
-          <S.WireSearchBox />
-          <S.WireSearchBox__icon />
-        </S.Wrapper>
-        <TimeLineList />
-      </S.Container>
-    </NavTemplate>
+    <S.Container>
+      <S.Wrapper>
+        <S.TextBox>
+          <S.MiddleText>중고차를 고민하고 있을 때</S.MiddleText>
+          <S.MiddleText>차 관리 상태를 확인하고 싶을 때</S.MiddleText>
+          <S.PintText>중고차 들의 자기소개를 봐보자!</S.PintText>
+        </S.TextBox>
+        <S.GoBtn to="/search">차기소개서 확인하기</S.GoBtn>
+      </S.Wrapper>
+    </S.Container>
   );
 }
--- src/pages/Main/style.jsx
+++ src/pages/Main/style.jsx
@@ -1,19 +1,18 @@
 import styled from 'styled-components';
+import background from 'image/home2.png';
+import icon from 'image/icon/arrow_white.svg';
+import { Link } from 'react-router-dom';
 
-const headerHeight = 50;
-const searchIconSize = 30;
 
 export const Container = styled.div`
   display: flex;
   width: 100%;
-
   height: 100%;
 
-  background: #ededed;
+  background: url(${background});
+  background-size: contain;
+  background-repeat : no-repeat;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  justify-content: center;
 `;
 
 export const Wrapper = styled.div`
@@ -22,27 +21,52 @@ export const Wrapper = styled.div`
   width: 50%;
   height: 50px;
   position: relative;
+  top: 25%;
+  left: 15%;
+  flex-direction: column;
 `;
 
-export const WireSearchBox = styled.input`
-  min-width: 150px;
-  width: 100%;
-  height: 100%;
-  margin: auto;
-  background: #c4c4c4;
-  border-radius: 40px;
-  padding-left: calc(${searchIconSize}px * 2 - 5px);
-  font-size: 18px;
-  border: 0;
+export const TextBox = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
-export const WireSearchBox__icon = styled.div`
-  position: absolute;
-  left: 0;
-  width: ${searchIconSize}px;
-  height: ${searchIconSize}px;
-  background: #a4a4a4;
-  margin-top: calc(calc(50px - ${searchIconSize}px) / 2);
-  margin-left: ${searchIconSize / 2}px;
-  border-radius: 20px;
+export const MiddleText = styled.label`
+  font-size: 2vw;
+  font-weight: 700;
+  color: #5a5a5a;
+  margin-bottom: 12px;
+`;
+
+export const PintText = styled.label`
+  font-size: 2.5vw;
+  font-weight: 700;
+  color: #5A5A5A;
+  margin: 3vh 0;
+`;
+
+export const GoBtn = styled(Link)`
+  display: flex;
+  width: 20vw;
+  line-height: 50px;
+  background: linear-gradient(270.15deg, #0087FF 4.66%, #00C3FF 100.81%);
+  color: white;
+  margin-top: 30px;
+  justify-content: center;
+  border-radius: 60.5px;
+  position: relative;
+  font-size: 1.3vw;
+
+  &:after {
+    content: '';
+    display: flex;
+    width: 14px;
+    height: 22px;
+    background: url(${icon});
+    background-size: contain;
+    background-repeat : no-repeat;
+    margin: auto 0;
+    margin-left: 3vw;
+    margin-right: -1vw;
+  }
 `;
