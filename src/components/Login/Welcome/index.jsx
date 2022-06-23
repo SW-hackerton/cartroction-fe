639c5f084edc961556c65b0fafb065ede04ad23f
유주현 <gus6359@gmail.com>
2022-06-24 00:32:37 +0900

    feat: Welcome page apply user type

--- src/components/Login/Welcome/index.jsx
+++ src/components/Login/Welcome/index.jsx
@@ -3,12 +3,14 @@ import * as S from './style';
 
 export function Welcome() {
   const { type } = useSelector(state => state.userType);
-
+  const getMessage = (name = '홍길동') => {
+    return `안녕하세요 ${name} ${type ? '정비사' : ''} 님 반갑습니다!`
+  }
   return (
     <S.Container to={type ? '/manager' : '/search'}>
       <S.Wrapper>
         <S.ProfileImg></S.ProfileImg>
-        <S.WelcomeMsg>{'안녕하세요 ~~ 님 반갑습니다!'}</S.WelcomeMsg>
+        <S.WelcomeMsg>{getMessage()}</S.WelcomeMsg>
       </S.Wrapper>
     </S.Container>
   );
